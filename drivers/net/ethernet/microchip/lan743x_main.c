2f58ef6117b30fccd8583c894348c178c0eae131
jmills <jmills@emacinc.com>
2022-02-22 17:36:45 -0600

    update the lan743x_phy_link_status_change function to try and use older phy stack and not crash

--- drivers/net/ethernet/microchip/lan743x_main.c
+++ drivers/net/ethernet/microchip/lan743x_main.c
@@ -949,7 +949,6 @@ static void lan743x_phy_link_status_change(struct net_device *netdev)
 {
 	struct lan743x_adapter *adapter = netdev_priv(netdev);
 	struct phy_device *phydev = netdev->phydev;
-	u32 data;
 
 	phy_print_status(phydev);
 	if (phydev->state == PHY_RUNNING) {
@@ -957,44 +956,12 @@ static void lan743x_phy_link_status_change(struct net_device *netdev)
 		int remote_advertisement = 0;
 		int local_advertisement = 0;
 
-		data = lan743x_csr_read(adapter, MAC_CR);
-
-		/* set interface mode to RGMII */
-        data &= ~MAC_CR_MII_EN_;
-		
-		/* set duplex mode */
-		if (phydev->duplex)
-			data |= MAC_CR_DPX_;
-		else
-			data &= ~MAC_CR_DPX_;
-
-		/* set bus speed
-		switch (phydev->speed) {
-		case SPEED_10:
-			data &= ~MAC_CR_CFG_H_;
-			data &= ~MAC_CR_CFG_L_;
-		break;
-		case SPEED_100:
-        */
-        data &= ~MAC_CR_CFG_H_;
-        data |= MAC_CR_CFG_L_;
-		/*
-            break;
-		case SPEED_1000:
-			data |= MAC_CR_CFG_H_;
-			data &= ~MAC_CR_CFG_L_;
-		break;
-		}
-		*/
-        
-		lan743x_csr_write(adapter, MAC_CR, data);
-
 		memset(&ksettings, 0, sizeof(ksettings));
 		phy_ethtool_get_link_ksettings(netdev, &ksettings);
 		local_advertisement =
-			linkmode_adv_to_mii_adv_t(phydev->advertising);
+			ethtool_adv_to_mii_adv_t(phydev->advertising);
 		remote_advertisement =
-			linkmode_adv_to_mii_adv_t(phydev->lp_advertising);
+			ethtool_adv_to_mii_adv_t(phydev->lp_advertising);
 
 		lan743x_phy_update_flowcontrol(adapter,
 					       ksettings.base.duplex,
